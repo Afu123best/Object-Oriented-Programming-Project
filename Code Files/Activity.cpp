a33cd6555bf7b99cb17be9fb5741063d62a9dc6c
AR-MURAD <l250608@lhr.nu.edu.pk>
2026-04-30 10:59:36 +0500

    Add files via upload

---
+++ Code Files/Activity.cpp
@@ -0,0 +1,43 @@
+#include "Activity.h"
+#include "Post.h"
+
+Activity::Activity(string id, string description, Date* d, Entity* posted_by, int type, string value)
+: Post(id, description, d, posted_by)
+{
+    this->type = type;
+    this->value = value;
+}
+
+int Activity::get_type()
+{
+    return type;
+}
+string Activity::get_value()
+{
+    return value;
+}
+
+void Activity::display()
+{
+    cout << "--- " << get_posted_by()->get_name() << " is ";
+    if (type == 1)
+    {
+        cout << "feeling " << value << endl;
+    }
+    else if (type == 2)
+    {
+        cout << "thinking about " << value << endl;
+    }
+    else if (type == 3)
+    {
+        cout << "making " << value << endl;
+    }
+    else if (type == 4)
+    {
+        cout << "celebrating " << value << endl; 
+    }
+    cout << "\"" << get_description() << "\" ... (";
+    get_shared_date()->display();
+    cout << ")" << endl;
+    display_comments();
+}
---
+++ Code Files/Activity.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "Post.h"
+#include <string>
+#include <iostream>
+using namespace std;
+
+class Activity : public Post
+{
+    int type;
+    string value;
+public:
+    Activity(string id, string description, Date* d, Entity* posted_by,int type, string value);
+
+    int get_type();
+    string get_value();
+    
+    void display() override;
+};
